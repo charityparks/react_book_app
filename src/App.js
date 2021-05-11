a2be11c631c93951016e97423ed2d2237e0dccd1
Charity Parks <tuppercharity@gmail.com>
2021-05-10 21:12:05 -0400

    chapter 2 complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Products from './Products';
 
 class App extends Component {
   render() {
@@ -6,6 +7,7 @@ class App extends Component {
       <div>
         <h1>
         My React Book App!</h1>
+        <Products />
       </div>
     );
   }
--- src/Products.js
+++ src/Products.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class Products extends Component {
     render() {
+        const products = ["learning React", "pro React", "Beginning React"];
+        const listProducts = products.map((product) => 
+            <li key={product.toString()}>{product}</li>
+        );
         return (
             <div>
-                <h2>
-                    Products
-                </h2>
+                <ul>{listProducts}</ul>
             </div>
         );
     }
