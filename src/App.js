df7e5ded1f83ea878e37472ffa30140c4d6a7254
Charity Parks <tuppercharity@gmail.com>
2021-05-18 18:53:18 -0400

    Added Rating

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import Products from './Products';
 import { Button } from 'react-bootstrap';
+import Rating from './Rating';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>
+        <Rating rating="1"/>
+        <Rating rating="2"/>
+        <Rating rating="3"/>
+        <Rating rating="4"/>
+        <Rating rating="5"/>
+        {/* <h1>
         My React Book App!</h1>
         <Products />
-        <Button>Default</Button>
+        <Button variant="danger">Default</Button> */}
       </div>
     );
   }
