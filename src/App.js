ae92c5457ab46f9c8f01cf4bed849b7ed3e02749
Charity Parks <tuppercharity@gmail.com>
2021-05-18 18:01:12 -0400

    Added bootstrap and button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Products from './Products';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <h1>
         My React Book App!</h1>
         <Products />
+        <Button>Default</Button>
       </div>
     );
   }
