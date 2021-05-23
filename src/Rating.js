449d3cb1f95b1449409ba589ba674c263f15dfb4
Charity Parks <tuppercharity@gmail.com>
2021-05-23 17:14:26 -0400

    changed color of rating

--- src/Rating.js
+++ src/Rating.js
@@ -13,7 +13,7 @@ class Rating extends Component {
   
      render() {
         return (
-            <div>
+            <div style={styles.starStyle}>
                <h1>Rating: {this.state.rating}</h1>
                {this.state.rating >= 1 ? (
                    <IoIosStar onClick={this.handleClick.bind(this,1)}/>
@@ -50,3 +50,9 @@ class Rating extends Component {
     }
 }
 export default Rating;
+
+const styles={
+    starStyle:{
+        color: 'orange'
+    }
+}
