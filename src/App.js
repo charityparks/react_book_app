17e02262cb4a6b55d0b10e4f6b1a0aa10c5b944b
Charity Parks <tuppercharity@gmail.com>
2021-05-29 17:37:52 -0400

    added product to appjs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import Products from './Products';
-import { Button } from 'react-bootstrap';
-import Rating from './Rating';
+// import { Button } from 'react-bootstrap';
+// import Rating from './Rating';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Rating rating="1"/>
+      <div className="App">
+        <Products />
+
+        {/* <Rating rating="1"/>
         <Rating rating="2"/>
         <Rating rating="3"/>
         <Rating rating="4"/>
-        <Rating rating="5"/>
+        <Rating rating="5"/> */}
         {/* <h1>
         My React Book App!</h1>
         <Products />
