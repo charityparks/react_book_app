bb0bc509a8e9333f89c9f1399dccb1cc1d993310
Charity Parks <tuppercharity@gmail.com>
2021-06-05 14:03:20 -0400

    second save Userform up to form

--- src/UserForm.js
+++ src/UserForm.js
@@ -12,8 +12,29 @@ class UserForm extends Component {
                 <Formik
                 initialValues={ { email: '', password: '' }}
                 validate={values => {
-                    
+                    let errors = {};
+                    if (!values.email) {
+                        errors.email = 'Required';
+                    }   else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        errors.email = 'Invalid email address';
+                    }
+                    return errors;
                 }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+                >
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <Field type=
+                        </Form>
+                    )}
+                </Formik>
             </div>
         )
     }
