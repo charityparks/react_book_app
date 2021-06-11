26483a3ee105318ba2ee6254ef2fb9651dfd9624
Charity Parks <tuppercharity@gmail.com>
2021-06-11 19:04:44 -0400

    added more password and email validation

--- src/UserForm.js
+++ src/UserForm.js
@@ -10,15 +10,24 @@ class UserForm extends Component {
             <div>
                 <h1>Any place in your app!</h1>
                 <Formik
-                initialValues={ { email: '', password: '' }}
-                validate={values => {
-                    let errors = {};
-                    if (!values.email) {
-                        errors.email = 'Required';
+                    initialValues={ { email: '', password: '' }}
+                    validate={values => {
+                        let errors = {};
+                        if (!values.email) {
+                            errors.email = 'Required';
                     }   else if (
                         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                     ) {
                         errors.email = 'Invalid email address';
+                    }
+                    else if (values.email.length < 10) {
+                        errors.email =  'Email address too short';
+                    }
+                    if (!values.password) {
+                        errors.password = 'Required';
+                    }
+                    else if (values.password.length < 8) {
+                        errors.password = 'Password too short';
                     }
                     return errors;
                 }}
