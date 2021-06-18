d9374fb66529a8e9a5a9472fbb13912f979e9c13
Charity Parks <tuppercharity@gmail.com>
2021-06-18 18:50:42 -0400

    installed react-loading

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import axios from 'axios'; // npm install axios
+import ReactLoading from 'react-loading';
 
 class GitHub extends Component {
 
     constructor() {
         super();
+        this.state = {
+            isLoading : true
+        };
     }
+
 componentDidMount() {
     this.getGitHubData('greg');
 }
@@ -13,6 +18,9 @@ componentDidMount() {
     getGitHubData (_searchTerm) {
         axios.get ("https://api.github.com/search/users?q="+_searchTerm)
         .then (res => {
+            this.setState ({
+                isLoading : false,
+            })
             console.log(res.data.items);
         });
     }
@@ -20,6 +28,8 @@ componentDidMount() {
     render() {
         return (
             <div>
+                { this.state.isLoading && 
+                <h4>Getting data...</h4> }
             </div>
         );
     }
