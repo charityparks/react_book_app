216eccf501d7aaee85148f9aa9e5d916ec34b7d7
Charity Parks <tuppercharity@gmail.com>
2021-07-03 20:46:43 -0400

    changed App.js to work on routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import Products from './Products';
 import UserForm from './UserForm';
 import GitHub from './GitHub';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Products /> 
-        <UserForm />
-        <GitHub />     
+      <div>
+        <Header /> 
       </div>
+
+      // <div className="App">
+      //   <Products /> 
+      //   <UserForm />
+      //   <GitHub />     
+      // </div>
     );
   }
 }
 
 export default App;
+
+class Header extends
