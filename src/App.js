7828a0e816f54cdae654e1cae3bd1f26dd076175
Charity Parks <tuppercharity@gmail.com>
2021-07-16 18:56:05 -0400

    Added About Route

--- src/App.js
+++ src/App.js
@@ -35,12 +35,14 @@ class Header extends Component {
               <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/github">GitHub</Nav.Link>
+                <Nav.Link href="/About"> About</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
           <Switch>
             <Route path="/github" component={GitHub} />
             <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
             <Route path="/*" component={NotFound} />
           </Switch>
         </div>
